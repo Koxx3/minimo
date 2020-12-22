f60fa52d3c6de76fb1cc19414dffb7ed5e4c828d
Koxx3 <koxx33@gmail.com>
2020-12-22 14:16:49 +0100

    add temp varaible

--- burn_efuse_gpio12.bat
+++ burn_efuse_gpio12.bat
@@ -1,10 +1,12 @@
-@echo off
+;//@echo off
+
+SETLOCAL enableextensions
+
 IF "%1"=="" GOTO :Continue
     pip install esptool
-    cd c:\temp\
+    cd %temp%
     git clone https://github.com/espressif/esptool.git
-    C:\temp\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
-
+    %TEMP%\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
 :Continue
 IF "%1"=="" echo You must specify COM port (COM19 for exemple)
 
