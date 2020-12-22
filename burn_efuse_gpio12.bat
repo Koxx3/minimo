e035aea0511bb6f0e2b829fd2eb90136fb49adc3
Koxx3 <koxx33@gmail.com>
2020-12-22 14:08:10 +0100

    efuse script : check parameters

--- burn_efuse_gpio12.bat
+++ burn_efuse_gpio12.bat
@@ -1,4 +1,11 @@
-pip install esptool
-cd c:\temp\
-git clone https://github.com/espressif/esptool.git
-C:\temp\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
+@echo off
+IF "%1"=="" GOTO :Continue
+    pip install esptool
+    cd c:\temp\
+    git clone https://github.com/espressif/esptool.git
+    C:\temp\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
+
+:Continue
+IF "%1"=="" echo You must specify COM port (COM19 for exemple)
+
+pause
