e6424208831bc42c3a9cbe398a808902393337c9
Koxx3 <koxx33@gmail.com>
2020-12-22 13:55:17 +0100

    Update burn_efuse_gpio12.bat

--- burn_efuse_gpio12.bat
+++ burn_efuse_gpio12.bat
@@ -1,3 +1,3 @@
 cd c:\temp\
 git clone https://github.com/espressif/esptool.git
-C:\temp\tool-esptoolpy\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
+C:\temp\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
