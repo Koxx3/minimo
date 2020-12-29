3729310a45d49ea13ab282896b286529cf2f5f0a
Raphaël <leraphael2600@hotmail.fr>
2020-12-29 12:29:30 +0100

    esptool

--- burn_efuse_gpio12.bat
+++ burn_efuse_gpio12.bat
@@ -4,9 +4,7 @@ SETLOCAL enableextensions
 
 IF "%1"=="" GOTO :Continue
     pip install esptool
-    cd %temp%
-    git clone https://github.com/espressif/esptool.git
-    %TEMP%\esptool\espefuse.py -p %1 --do-not-confirm set_flash_voltage 3.3V
+    espefuse.exe -p %1 --do-not-confirm set_flash_voltage 3.3V
 :Continue
 IF "%1"=="" echo You must specify COM port (COM19 for exemple)
 
