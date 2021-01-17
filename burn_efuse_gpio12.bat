dc59bcca50811d84ac9a88c8768a6e4a50f3d48c
Koxx3 <koxx33@gmail.com>
2021-01-17 13:18:33 +0100

    try to fix efuse burn script

--- burn_efuse_gpio12.bat
+++ burn_efuse_gpio12.bat
@@ -1,10 +1,11 @@
-;//@echo off
+@echo off
 
 SETLOCAL enableextensions
 
 IF "%1"=="" GOTO :Continue
     pip install esptool
-    espefuse.exe -p %1 --do-not-confirm set_flash_voltage 3.3V
+    python -m espefuse -p %1 --do-not-confirm set_flash_voltage 3.3V
+    
 :Continue
 IF "%1"=="" echo You must specify COM port (COM19 for exemple)
 
