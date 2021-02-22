ed698459645bbc5ad33a79040d5dae033172ff06
Koxx3 <koxx33@gmail.com>
2021-02-22 22:40:34 +0100

    new version management

--- extra_script.py
+++ extra_script.py
@@ -1,6 +1,8 @@
 import requests, sys, json, os
 from os.path import basename
 from shutil import copyfile
+import re
+import time, datetime
 
 Import("env")
 
@@ -16,6 +18,10 @@ port = project_config.get("common", "JsonPort")
 bin = project_config.get("common", "JsonBin")
 
 
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__() # return "{}-{}-{}".format(o.year, o.month, o.day)
+
 def after_build(source, target, env):
     print("----------------------------------------------------------------------------------------------------------")
     firmware_path = str(source[0])
@@ -24,7 +30,7 @@ def after_build(source, target, env):
     dstFilename = "firmware_v" + version + ".bin"
     finalDstFullPath = dstMainPath + dstFilename
     
-    if os.path.isfile(finalDstFullPath):
+    if False: #os.path.isfile(finalDstFullPath):
         print ("This firmware version already exists ... increase the firmware_version or delete the files if you want to generate a new version.")
     else:
         print("Copying bin file...")
@@ -32,6 +38,9 @@ def after_build(source, target, env):
 
         os.makedirs(os.path.dirname(dstMainPath), exist_ok=True)
         copyfile(firmware_path , finalDstFullPath)
+
+        modified = os.path.getmtime(finalDstFullPath)
+        dateFile = datetime.datetime.fromtimestamp(modified)
 
         # Generating JSON version file
         print("Generating JSON version file...")
@@ -41,10 +50,42 @@ def after_build(source, target, env):
         payload['host'] = host
         payload['port'] = port
         payload['bin'] = bin + envName + "/" + dstFilename
+        payload['date'] = dateFile
+
+        try:
+            with open(dstMainPath + "firmware_v" + version + ".json", 'w') as outfile:
+                json.dump(payload, outfile, default = myconverter)
+        except:
+            print("[ERROR] Error when generating the JSON version file...")
+
+        fullPayload = {}
+        fullPayload['versions'] = []
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(dstMainPath):
+            for file in f:
+                if file.endswith(".bin"):
+                    filePath = os.path.join(r, file)
+                    print(filePath)
+                    fileName = os.path.basename(filePath)
+                    modified = os.path.getmtime(filePath)
+                    # remove file extension
+                    versionNumber = os.path.splitext(fileName)[0]
+                    # split with 'v"
+                    versionNumber = re.split('v', versionNumber)[1]
+                    
+                    versionElement = {}
+                    versionElement['type'] = envName
+                    versionElement['version'] = versionNumber
+                    versionElement['host'] = host
+                    versionElement['port'] = port
+                    dateFile = datetime.datetime.fromtimestamp(modified)
+                    versionElement['date'] = dateFile
+                    versionElement['bin'] = bin + envName + "/" + fileName
+                    fullPayload['versions'].append(versionElement)
 
         try:
             with open(dstMainPath + "firmware.json", 'w') as outfile:
-                json.dump(payload, outfile)
+                json.dump(fullPayload, outfile, default = myconverter)
         except:
             print("[ERROR] Error when generating the JSON version file...")
 
